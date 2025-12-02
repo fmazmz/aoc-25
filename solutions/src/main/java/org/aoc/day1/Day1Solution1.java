538b17f27ee266698b27767a484fb9a557b24846
WHITEROSE <firasmoussa60@gmail.com>
2025-12-02 21:35:38 +0100

    day1

---
+++ solutions/src/main/java/org/aoc/day1/Day1Solution1.java
@@ -0,0 +1,62 @@
+package org.aoc.day1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+
+/**
+ * <p>
+ * Advent of Code Day 1: Secret Entrance
+ * </p>
+ * <p>
+ * Reads rotations from CLI input (one per line), simulates a circular dial from 0 to 99
+ * starting at 50, and counts how many times the dial lands at 0.
+ * End input with Ctrl+D (Linux/Mac) or Ctrl+Z + Enter (Windows).
+ * </p>
+ *
+ * <p>Usage:</p>
+ * <ul>
+ *   <li>mvn compile</li>
+ *   <li>java -cp org.aoc.day1.Day1Solution1 org.example.Day1</li>
+ *   <li>Paste raw input from AOC::2025/day/1/input</li>
+ * </ul>
+ */
+
+public class Day1Solution1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> rotations = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().trim();
+            if (line.isEmpty()) continue;
+            rotations.add(line);
+        }
+        run(rotations);
+    }
+
+    static void run(List<String> rotations) {
+
+        List<String> sequence = new ArrayList<>(rotations);
+        int currentIdx = 50;
+        int zeros = 0;
+
+        for (String string : sequence) {
+            String s = string.substring(0, 1);
+            int rotation = Integer.parseInt(string.substring(1));
+
+            // Left rotation
+            if (Objects.equals(s, "L")) currentIdx = (currentIdx - rotation) % 100;
+                // Right rotation
+            else currentIdx = (currentIdx + rotation) % 100;
+
+            // handle negative rotations
+            if (currentIdx < 0) currentIdx += 100;
+
+            if (currentIdx == 0) zeros++;
+        }
+        System.out.println("Password: " + zeros);
+    }
+}
